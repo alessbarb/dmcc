ca9e215e1c5d3548c38d4a0e21f645398b72fb66
Alessandro Barbagallo <alessbarb@gmail.com>
2026-07-11 01:20:48 +0200

    Load graph mobile refinements

--- src/frontend/shared/components/AppFooter.tsx
+++ src/frontend/shared/components/AppFooter.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "../i18n/useTranslation.js";
 import { institutionalContact } from "../../institutional/institutionalContent.js";
 import "../styles/appFooter.css";
 import "../../dm/layouts/campaign-route-transitions.css";
+import "../../dm/graph/graph-mobile.css";
 
 const APP_VERSION = "0.1.0";
 
