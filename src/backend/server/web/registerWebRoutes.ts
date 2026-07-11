58811510305113f68d9a91bb2972949c2ba3dbc7
Alessandro Barbagallo <alessbarb@gmail.com>
2026-07-11 13:11:57 +0200

    Register canonical command center routes

--- src/backend/server/web/registerWebRoutes.ts
+++ src/backend/server/web/registerWebRoutes.ts
@@ -4,7 +4,7 @@ import { registerAccountWebRoutes } from "./routes/accountWebRoutes.js";
 import { registerAuthWebRoutes } from "./routes/authWebRoutes.js";
 import { registerCampaignWebRoutes } from "./routes/campaignWebRoutes.js";
 import { registerCanvasWebRoutes } from "./routes/canvasWebRoutes.js";
-import { registerDmDashboardWebRoutes } from "./routes/dmDashboardWebRoutes.js";
+import { registerCommandCenterWebRoutes } from "./routes/commandCenterWebRoutes.js";
 import { registerHealthWebRoutes } from "./routes/healthWebRoutes.js";
 import { registerInvitationWebRoutes } from "./routes/invitationWebRoutes.js";
 import { registerLiveTableWebRoutes } from "./routes/liveTableWebRoutes.js";
@@ -23,7 +23,7 @@ export function registerWebRoutes(server: FastifyInstance, options: WebRoutesOpt
   void registerHealthWebRoutes(server);
   void registerCampaignWebRoutes(server);
   void registerCanvasWebRoutes(server);
-  void registerDmDashboardWebRoutes(server);
+  void registerCommandCenterWebRoutes(server);
   void registerInvitationWebRoutes(server);
   void registerLiveTableWebRoutes(server);
   void registerPlayerPortalWebRoutes(server);
