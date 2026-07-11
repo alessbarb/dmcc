3cf6bcc30a94f5fa612772185cfea99f21b1177a
Alessandro Barbagallo <alessbarb@gmail.com>
2026-07-11 13:01:44 +0200

    Register premade campaign web routes

--- src/backend/server/web/registerWebRoutes.ts
+++ src/backend/server/web/registerWebRoutes.ts
@@ -9,6 +9,7 @@ import { registerHealthWebRoutes } from "./routes/healthWebRoutes.js";
 import { registerInvitationWebRoutes } from "./routes/invitationWebRoutes.js";
 import { registerLiveTableWebRoutes } from "./routes/liveTableWebRoutes.js";
 import { registerPlayerPortalWebRoutes } from "./routes/playerPortalWebRoutes.js";
+import { registerPremadeCampaignWebRoutes } from "./routes/premadeCampaignWebRoutes.js";
 import { registerRulesWebRoutes } from "./routes/rulesWebRoutes.js";
 import { registerSearchWebRoutes } from "./routes/searchWebRoutes.js";
 
@@ -28,6 +29,7 @@ export function registerWebRoutes(server: FastifyInstance, options: WebRoutesOpt
   void registerInvitationWebRoutes(server);
   void registerLiveTableWebRoutes(server);
   void registerPlayerPortalWebRoutes(server);
+  void registerPremadeCampaignWebRoutes(server);
   void registerRulesWebRoutes(server);
   void registerSearchWebRoutes(server);
   server.register(registerAssetRoutes, { assetsDir: options.assetsDir });
