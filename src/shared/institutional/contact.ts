a7cc62f94ca656d462c79d4441fa3bab94f1c3b8
Alessandro Barbagallo <alessbarb@gmail.com>
2026-07-11 11:25:02 +0200

    Remove institutional contact alias

--- src/shared/institutional/contact.ts
+++ src/shared/institutional/contact.ts
@@ -2,5 +2,3 @@ export const institutionalContact = {
   email: "dmcampaigncompanion@gmail.com",
   github: "https://github.com/alessbarb/DMCC",
 } as const;
-
-export const publicInstitutionalContact = institutionalContact;
