ef7ef884e614a53eff2212c4b03377e82bf47a96
Alessandro Barbagallo <alessbarb@gmail.com>
2026-07-16 02:15:56 +0200

    Mount entity detail Escape behavior

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router.js";
 import { I18nProvider } from "./shared/i18n/I18nProvider.js";
+import { EntityDetailEscapeBehavior } from "./shared/components/EntityDetailEscapeBehavior.js";
 import { PwaUpdateBanner } from "./shared/components/PwaUpdateBanner.js";
 import { Watermark } from "./shared/components/Watermark.js";
 import { ImageFocalPointBehavior } from "./shared/images/ImageFocalPointBehavior.js";
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider>
       <RouterProvider router={router} />
+      <EntityDetailEscapeBehavior />
       <ImageFocalPointBehavior />
       <Watermark />
       <PwaUpdateBanner />
